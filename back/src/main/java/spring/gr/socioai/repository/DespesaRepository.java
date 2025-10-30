0283ab29d609ff74192d9a65f632b156aacf3ea2
DevIsper <gabrielisperb@gmail.com>
2025-10-30 11:06:52 -0300

    remove 🔥: remove Despesa do sistema, visando refatoração do modelo

--- back/src/main/java/spring/gr/socioai/repository/DespesaRepository.java
+++
@@ -1,9 +0,0 @@
-package spring.gr.socioai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import spring.gr.socioai.model.DespesaEntity;
-
-@Repository
-public interface DespesaRepository extends JpaRepository<DespesaEntity, Long> {
-}
